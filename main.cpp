4313ee1b39b3c7b82c4cf664de1ba5f7b35bad2c
nico-fico <nikoloban04@gmail.com>
2024-04-08 22:51:03 +0200

    proto move function

--- main.cpp
+++ main.cpp
@@ -2,17 +2,43 @@
 #include <iostream>
 
 struct BoardState {
-  std::array<char, 64> board{-4, -2, -3, -5, -6, -3, -2, -4,  //
-                             -1, -1, -1, -1, -1, -1, -1, -1,  //
-                             0,  0,  0,  0,  0,  0,  0,  0,   //
-                             0,  0,  0,  0,  0,  0,  0,  0,   //
-                             0,  0,  0,  0,  0,  0,  0,  0,   //
-                             0,  0,  0,  0,  0,  0,  0,  0,   //
-                             1,  1,  1,  1,  1,  1,  1,  1,   //
-                             4,  2,  3,  5,  6,  3,  2,  4};
+  std::array<int, 64> board{-4, -2, -3, -5, -6, -3, -2, -4,  //
+                            -1, -1, -1, -1, -1, -1, -1, -1,  //
+                            0,  0,  0,  0,  0,  0,  0,  0,   //
+                            0,  0,  0,  0,  0,  0,  0,  0,   //
+                            0,  0,  0,  0,  0,  0,  0,  0,   //
+                            0,  0,  0,  0,  0,  0,  0,  0,   //
+                            1,  1,  1,  1,  1,  1,  1,  1,   //
+                            4,  2,  3,  5,  6,  3,  2,  4};
 
   std::array<bool, 2> white_can_castle{true, true};
   std::array<bool, 2> black_can_castle{true, true};
 };
 
-int main() { BoardState board; }
+struct Move {
+  char start_square;
+  char land_square;
+};
+
+void makeMove(BoardState& board, Move move) {
+  auto start = board.board.begin() + move.start_square;
+  auto land = board.board.begin() + move.land_square;
+
+  *land = *start;
+  *start = 0;
+}
+
+void showBoard(BoardState& board) {
+  for (auto it = board.board.begin(); it != board.board.end(); ++it) {
+    std::cout << *it << ' ';
+    if ((it - board.board.begin() + 1) % 8 == 0) {
+      std::cout << '\n';
+    }
+  }
+}
+
+int main() {
+  BoardState board;
+  makeMove(board, Move{0, 26});
+  showBoard(board);
+}
